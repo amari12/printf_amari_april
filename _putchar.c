5be848fbe40db319b62303aae299f492fdb31640
Amari <amarikotze@gmail.com>
2023-04-22 11:19:37 -0700

    separated putchar

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes char to standard output
+ * @c: char
+ * Return: succes - 1, error - (-1)
+ */
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
+
